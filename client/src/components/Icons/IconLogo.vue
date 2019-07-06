0bb92c3549d962f867852737b8617751ad05221d
60° <hatsumi.yamane.4609@gmail.com>
2019-07-07 04:25:18 +0900

    add icons

---
+++ client/src/components/Icons/IconLogo.vue
@@ -0,0 +1,29 @@
+<template functional>
+  <svg width="41" height="33" viewBox="0 0 41 33" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <rect x="1" y="25.2998" width="28" height="7" rx="1" :fill="props.color" />
+    <rect
+      x="32.0345"
+      width="31.9759"
+      height="7.03679"
+      rx="1"
+      transform="rotate(74.2986 32.0345 0)"
+      :fill="props.color"
+    />
+    <rect y="13.2998" width="25" height="7" rx="1" :fill="props.color" />
+  </svg>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator'
+
+@Component({
+  components: {},
+})
+export default class IconLogo extends Vue {
+  @Prop({ type: String, default: 'var(--logo-gray)' })
+  public color!: string
+}
+</script>
+
+<style lang="sass">
+</style>
---
+++ client/src/components/Icons/IconTsundoku.vue
@@ -0,0 +1,22 @@
+<template functional>
+  <svg width="46" height="40" viewBox="0 0 46 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <rect x="2.83081" y="30.1384" width="35.3846" height="8.49231" rx="1" :fill="props.color" />
+    <rect y="1" width="35.3846" height="8.49231" rx="1" :fill="props.color" />
+    <rect x="14.1538" y="15.5693" width="31.8462" height="8.49231" rx="1" :fill="props.color" />
+  </svg>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator'
+
+@Component({
+  components: {},
+})
+export default class IconTsundoku extends Vue {
+  @Prop({ type: String, default: 'var(--tsundoku-red)' })
+  public color!: string
+}
+</script>
+
+<style lang="sass">
+</style>

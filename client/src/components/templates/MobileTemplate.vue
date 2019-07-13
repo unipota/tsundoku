4ec42f664c5dbdf8d6826060ff7bcdef408c5b99
60° <hatsumi.yamane.4609@gmail.com>
2019-07-13 19:42:53 +0900

    DesktopTemplate, MobileTemplate を作成、ウィンドウサイズによってTemplateを変更

---
+++ client/src/components/templates/MobileTemplate.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+  div
+    slot(name="topBar")
+    slot(name="tabBar")
+    slot(name="routerView")
+
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+import MobileTabBar from '@/components/atoms/MobileTabBar.vue'
+
+@Component({
+  components: {
+    MobileTabBar
+  }
+})
+export default class MobileTemplate extends Vue {}
+</script>
+
+<style lang="sass"></style>

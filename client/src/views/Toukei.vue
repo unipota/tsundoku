364d3bbc15e80d225ef923302512984c0e0475e6
60° <hatsumi.yamane.4609@gmail.com>
2019-07-13 17:49:07 +0900

    TabBar toukeiを追加 (合わせてルーティングも変更)、selectedTabを$routeから読み取るようにした

---
+++ client/src/views/Toukei.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+  div
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component({
+  components: {}
+})
+export default class Toukei extends Vue {}
+</script>
+
+<style lang="sass"></style>

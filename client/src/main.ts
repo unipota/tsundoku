3c207bf72500cd38eb3ff39b39a3a91abb6c3066
60° <hatsumi.yamane.4609@gmail.com>
2019-07-20 04:16:09 +0900

    install i18n

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,11 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import i18n from './i18n'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  render: h => h(App) // eslint-disable-line @typescript-eslint/explicit-function-return-type
+  i18n,
+
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+  render: h => h(App)
 }).$mount('#app')

7ea9e6fd1222eb6bc84b66afed5b8f9dff14c73c
unipota <woz.eon@gmail.com>
2019-07-21 17:31:04 +0900

    wip

---
+++ client/src/components/atoms/FloatingAddTsundokuButton.vue
@@ -0,0 +1,34 @@
+<template lang="pug">
+  .floating-add-tsundoku-button.pointer
+   | +積む
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component({
+  components: {}
+})
+export default class FloatingAddTsundokuButton extends Vue {}
+</script>
+
+<style lang="sass" scoped>
+.floating-add-tsundoku-button
+  position: fixed
+  display: flex
+  align-items: center
+  right: 0
+  bottom: 94px + 18px
+  width: 94px
+  height: 60px
+  padding:
+    left: 16px
+  background-color: var(--tsundoku-red)
+  border:
+    radius: 100vw 0 0 100vw
+  color: var(--text-white)
+  font:
+    size: 24px
+    weight: bold
+  box-sizing: border-box
+</style>
--- client/src/views/Tsundoku.vue
+++ client/src/views/Tsundoku.vue
@@ -1,12 +1,14 @@
 <template lang="pug">
   div
     router-view
+    floating-add-tsundoku-button
 </template>
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator'
+import FloatingAddTsundokuButton from '@/components/atoms/FloatingAddTsundokuButton.vue'
 
-@Component({})
+@Component({ components: { FloatingAddTsundokuButton } })
 export default class Tsundoku extends Vue {}
 </script>
 

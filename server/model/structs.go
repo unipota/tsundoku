0d3d7d30f75f85a5becbe4af1457de486ffc07f7
とーふとふ <tohu.soy@gmail.com>
2019-07-21 16:11:30 +0900

    middleware

--- server/model/structs.go
+++ server/model/structs.go
@@ -7,86 +7,57 @@ import (
 	"github.com/jinzhu/gorm"
 )
 
-type Book struct {
-	ID             string    `gorm:"type:char(36);not null;primary_key"`
-	ISBN           string    `gorm:"type:char(13)"`
-	Title          string    `gorm:"type:char(60) not null;"`
-	Author         string    `gorm:"type:char(60);"`
-	TotalPages     int       `gorm:""`
-	RegularPrice   int       `gorm:""`
-	Caption        string    `gorm:"type:TEXT;"`
-	Publisher      string    `gorm:"type:char(60);"`
-	CoverImageUrl  string    `gorm:"type:char(200);"`
-	Memo           string    `gorm:"type:text;"`
-	PurchasedPrice int       `gorm:""`
-	DeviceID       string    `gorm:"char(36);not null"`
-	CreatedAt      time.Time `gorm:"precision:6"`
-	UpdatedAt      time.Time `gorm:"precision:6"`
+type Base struct {
+	ID        uuid.UUID  `gorm:"type:uuid;primary_key;"`
+	CreatedAt time.Time  `json:"created_at"`
+	UpdatedAt time.Time  `json:"update_at"`
+	DeletedAt *time.Time `sql:"index" json:"deleted_at"`
 }
 
-func (book *Book) BeforeCreate(scope gorm.Scope) error {
-	scope.SetColumn("ID", uuid.New())
-	return nil
+func (base *Base) BeforeCreate(scope *gorm.Scope) error {
+	id := uuid.New()
+	return scope.SetColumn("ID", id)
+}
+
+type Book struct {
+	Base
+	ISBN           string `gorm:"type:char(13)"`
+	Title          string `gorm:"type:char(60) not null;"`
+	Author         string `gorm:"type:char(60);"`
+	TotalPages     int    `gorm:""`
+	RegularPrice   int    `gorm:""`
+	Caption        string `gorm:"type:TEXT;"`
+	Publisher      string `gorm:"type:char(60);"`
+	CoverImageUrl  string `gorm:"type:char(200);"`
+	Memo           string `gorm:"type:text;"`
+	PurchasedPrice int    `gorm:""`
+	DeviceID       string `gorm:"char(36);not null"`
 }
 
 type Device struct {
-	ID        string    `gorm:"type:char(36);not null;primary_key"`
-	CreatedAt time.Time `gorm:"precision:6"`
-	UpdatedAt time.Time `gorm:"precision:6"`
-}
-
-func (device *Device) BeforeCreate(scope gorm.Scope) error {
-	scope.SetColumn("ID", uuid.New())
-	return nil
+	Base
 }
 
 type DeviceUser struct {
-	ID        string    `gorm:"type:char(36);not null;primary_key"`
-	DeviceID  string    `gorm:"type:char(36);not null;primary_key"`
-	UserID    string    `gorm:"type:char(36);not null;primary_key"`
-	CreatedAt time.Time `gorm:"precision:6"`
-	UpdatedAt time.Time `gorm:"precision:6"`
-}
-
-func (deviceUser *DeviceUser) BeforeCreate(scope gorm.Scope) error {
-	scope.SetColumn("ID", uuid.New())
-	return nil
+	Base
+	DeviceID string `gorm:"type:char(36);not null;primary_key"`
+	UserID   string `gorm:"type:char(36);not null;primary_key"`
 }
 
 type User struct {
-	ID        string    `gorm:"type:char(36);not null;primary_key"`
-	Name      string    `gorm:"type:char(40);not null;"`
-	CreatedAt time.Time `gorm:"precision:6"`
-	UpdatedAt time.Time `gorm:"precision:6"`
-}
-
-func (user *User) BeforeCreate(scope gorm.Scope) error {
-	scope.SetColumn("ID", uuid.New())
-	return nil
+	Base
+	Name string `gorm:"type:char(40);not null;"`
 }
 
 type Social struct {
-	ID         string    `gorm:"type:char(36);not null;primary_key"`
-	Type       string    `gorm:"type:char(40);not null;"`
-	Identifier string    `gorm:"type:char(40);"`
-	UserID     string    `gorm:"type:char(36);not null;"`
-	CreatedAt  time.Time `gorm:"precision:6"`
-	UpdatedAt  time.Time `gorm:"precision:6"`
-}
-
-func (social *Social) BeforeCreate(scope gorm.Scope) error {
-	scope.SetColumn("ID", uuid.New())
-	return nil
+	Base
+	Type       string `gorm:"type:char(40);not null;"`
+	Identifier string `gorm:"type:char(40);"`
+	UserID     string `gorm:"type:char(36);not null;"`
 }
 
 type BookHistory struct {
-	ID        string    `gorm:"type:char(36);not null;primary_key"`
-	BookID    string    `gorm:"type:char(36);not null;"`
-	CreatedAt time.Time `gorm:"precision:6"`
-	UpdatedAt time.Time `gorm:"precision:6"`
-}
-
-func (bookHistory *BookHistory) BeforeCreate(scope gorm.Scope) error {
-	scope.SetColumn("ID", uuid.New())
-	return nil
+	Base
+	BookID   string `gorm:"type:char(36);not null;"`
+	ReadPage int    `gorm:""`
 }

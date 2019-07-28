2935b75d64e4a6a093ff646cea8b457129243e4b
60° <hatsumi.yamane.4609@gmail.com>
2019-07-29 00:19:14 +0900

    デスクトップ表示に対応

--- client/src/views/Toukei.vue
+++ client/src/views/Toukei.vue
@@ -1,17 +1,20 @@
 <template lang="pug">
   div
     portal(to="modalView")
-      transition(name="modal-show")
+      transition(:name="`modal-show-${$store.state.viewType}`")
           router-view
 </template>
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator'
+import { ExStore } from 'vuex'
 
 @Component({
   components: {}
 })
-export default class Toukei extends Vue {}
+export default class Toukei extends Vue {
+  public $store!: ExStore
+}
 </script>
 
-<style lang="sass"></style>
+<style lang="sass" scoped></style>

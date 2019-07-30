188233288ab9c7ff2c6bfec974fbbebd336faa84
Yoya Mesaki <yoyamssk@gmail.com>
2019-07-30 23:16:54 +0900

    impl search

---
+++ client/src/components/molecules/ScanCard.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+  .scan-card
+    book-cover(:book="book")
+    span
+      | {{ book.isbn }}
+    span
+      | {{ book.title }}
+</template>
+
+<script lang="ts">
+import BookCover from '@/components/atoms/BookCover.vue'
+import { Vue, Component, Prop } from 'vue-property-decorator'
+import { BookRecord } from '../../types/Book'
+
+@Component({
+  components: {
+    BookCover
+  }
+})
+export default class ScanCard extends Vue {
+  @Prop({ type: Object, required: true })
+  private book!: BookRecord
+}
+</script>
+
+<style lang="sass">
+</style>

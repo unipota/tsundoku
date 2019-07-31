c9d4674689a7b03af44bdf245b2041ed753ec1a2
unipota <woz.eon@gmail.com>
2019-07-31 15:57:06 +0900

    add Privacy and Term view

---
+++ client/src/views/Privacy.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+  div
+    | privacy
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+import { ExStore } from 'vuex'
+
+@Component({
+  components: {}
+})
+export default class Privacy extends Vue {
+  public $store!: ExStore
+}
+</script>
+
+<style lang="sass" scoped></style>

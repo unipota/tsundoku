02ba0ef65072d34441bb25799eb5ab43e3694352
unipota <woz.eon@gmail.com>
2019-08-13 00:31:09 +0900

    fix

--- client/src/components/molecules/BookListItemProgressController.vue
+++ client/src/components/molecules/BookListItemProgressController.vue
@@ -3,8 +3,8 @@
     .book-list-item-progress-container
       .progress-wrap
         book-list-item-progress(
-          :readPages="readPages"
-          :totalPages="totalPages"
+          :readPages="book.readPages"
+          :totalPages="book.totalPages"
           :edit="recordActive" 
           :editedReadPages.sync="editedReadPages")
       .record-read-pages-button-wrap
@@ -12,7 +12,7 @@
       .check-button-wrap
         kidoku-button(@click="handleClickCheck")
     .progress-input-wrap(v-show="recordActive")
-      progress-input(:totalPages="totalPages" v-model="editedReadPages" @cancel="handleCancel")
+      progress-input(:totalPages="book.totalPages" v-model="editedReadPages" @cancel="handleCancel")
 </template>
 
 <script lang="ts">
@@ -33,26 +33,29 @@ import ProgressInput from '@/components/atoms/ProgressInput.vue'
   }
 })
 export default class BookListItemProgressController extends Vue {
-  @Prop({ type: String, required: true })
-  private id!: string
+  @Prop({ type: Object, required: true })
+  private book!: BookRecord
 
-  @Prop({ type: Number, required: true })
-  private readPages!: number
+  // @Prop({ type: String, required: true })
+  // private id!: string
 
-  @Prop({ type: Number, required: true })
-  private totalPages!: number
+  // @Prop({ type: Number, required: true })
+  // private readPages!: number
+
+  // @Prop({ type: Number, required: true })
+  // private totalPages!: number
 
   public $store!: ExStore
 
   recordActive: boolean = false
-  editedReadPages: number = this.readPages
+  editedReadPages: number = this.book.readPages
 
   handleClickRecord() {
     if (!this.recordActive) {
       this.recordActive = true
     } else {
       this.$store.commit('updateBookReadPages', {
-        id: this.id,
+        id: this.book.id,
         readPages: this.editedReadPages
       })
       this.recordActive = false
@@ -63,7 +66,7 @@ export default class BookListItemProgressController extends Vue {
 
   handleCancel() {
     this.recordActive = false
-    this.editedReadPages = this.readPages
+    this.editedReadPages = this.book.readPages
   }
 }
 </script>

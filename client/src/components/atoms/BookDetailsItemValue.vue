c3b3bf9aa5a7048d85d0a05e60342753da068f78
Yoya Mesaki <yoyamssk@gmail.com>
2019-08-17 17:58:50 +0900

    add memos

---
+++ client/src/components/atoms/BookDetailsItemValue.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+  .book-details-item-value(:style="style")
+    | {{ value }}
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator'
+
+@Component
+export default class BookDetailsItemValue extends Vue {
+  @Prop({ type: String, required: true })
+  private value!: string
+
+  @Prop({ type: String, required: true })
+  private color!: string
+
+  get style() {
+    return {
+      color: this.color
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+.book-details-item-value
+  color: $text-black
+  font-weight: bold
+</style>
